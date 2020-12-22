ae92f2d4358afc9581c8a3eaeaa3d8d277637448
nemo256 <neggazimedlamine@gmail.com>
2020-12-22 20:08:41 +0100

    Updated my fork of slstatus!

--- config.h
+++ config.h
@@ -64,10 +64,9 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	//{ datetime, "%s",           "%F %T" },
-	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
+	{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
     //{ netspeed_rx, "%sB/s  ", "enp0s3" },
-	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
-	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
-	{ datetime, "%s",           "%a %b %d %r" },
+	//{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
+	//{ ram_perc, "[RAM  %s%%]   ", NULL	      },
+	//{ datetime, "%s",           "%a %b %d %r" },
 };
