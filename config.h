b95eb9e9306e703fde47b91ab4007e3549a9b228
nemo256 <neggazimedlamine@gmail.com>
2021-01-10 12:31:27 +0100

    Debian updated for long term usage!

--- config.h
+++ config.h
@@ -63,9 +63,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ battery_perc, "[Battery  %s%%]",		"BAT0" },
+	{ vol_perc, "[ %s%%]",	"/dev/snd/pcmC0D3p" },
+	{ battery_perc, "[ %s%%]",	"BAT0" },
 	//{ wifi_perc, "W: (%3s%% on ", "wlp3s0" },
-    	//{ netspeed_rx, "%sB/s  ", "enp0s3" },
+    	{ netspeed_rx, "[%sB/s ]", "enp0s3" },
 	//{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
 	//{ ram_perc, "[RAM  %s%%]   ", NULL	      },
 	//{ datetime, "%s",           "%a %b %d %r" },
